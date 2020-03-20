3dd862fa6ed4a6b317dcefc7c912b83481531a9d
Maddie Peters <madeleine0906@gmail.com>
2020-03-20 14:13:51 +1000

    "Added text to password_generator."

---
+++ password_generator.py
@@ -0,0 +1,3 @@
+# A python programme to generate a random password for the user.
+# The user provides the length of the password. The program will return a
+# password that has a random combination of letters, numbers, and symbols.
